b10ed1c228214ed24753a8aaaf887f75a4043872
NotHydra <rizky.irswanda115@gmail.com>
2022-03-23 16:09:58 +0800

    Heroku Initialize

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 // app.use(express.urlencoded({ extended:false }));
 
-const port: number = 5000;
+const port: any = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response) => {
     res.redirect('/home');
