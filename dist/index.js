0940c12c6ca03415dfe5b8385c7cbc51569c1d9d
NotHydra <rizky.irswanda115@gmail.com>
2022-03-23 16:19:07 +0800

    Forgot To Build TS To JS

--- dist/index.js
+++ dist/index.js
@@ -8,11 +8,23 @@ const app = (0, express_1.default)();
 app.set('view engine', 'ejs');
 app.use(express_1.default.static('public'));
 // app.use(express.urlencoded({ extended:false }));
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.redirect('/home');
 });
 app.get('/home', (req, res) => {
     res.render('home', { title: 'Home' });
+});
+app.get('/stats', (req, res) => {
+    res.render('stats', { title: 'Stats' });
+});
+app.get('/upgrade', (req, res) => {
+    res.render('upgrade', { title: 'Upgrade' });
+});
+app.get('/history', (req, res) => {
+    res.render('history', { title: 'History' });
+});
+app.get('/setting', (req, res) => {
+    res.render('setting', { title: 'Setting' });
 });
 app.listen(port, () => { console.log(`Listening in http://localhost:${port}`); });
