272ff16cbeab5f57fae55150667f1a26fcffef3e
NotHydra <rizky.irswanda115@gmail.com>
2022-03-24 13:45:03 +0800

    Added Icon To Navigation Sidebar

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,36 @@ app.use(express.static('public'));
 
 const port: any = process.env.PORT || 5000;
 
+const list_of_page_attribute: Array<Array<string>> = [
+    ["home", "fa-home"],
+    ["stats", "fa-bars-progress"],
+    ["upgrade", "fa-angles-up"],
+    ["history", "fa-list-ul"],
+    ["setting", "fa-gear"]
+];
+
 app.get('/', (req: Request, res: Response) => {
     res.redirect('/home');
 });
 
 app.get('/home', (req: Request, res: Response) => {
-    res.render('home', { title: 'Home' });
+    res.render('home', { title: 'Home', list_of_page_attribute });
 });
 
 app.get('/stats', (req: Request, res: Response) => {
-    res.render('stats', { title: 'Stats' });
+    res.render('stats', { title: 'Stats', list_of_page_attribute });
 });
 
 app.get('/upgrade', (req: Request, res: Response) => {
-    res.render('upgrade', { title: 'Upgrade' });
+    res.render('upgrade', { title: 'Upgrade', list_of_page_attribute });
 });
 
 app.get('/history', (req: Request, res: Response) => {
-    res.render('history', { title: 'History' });
+    res.render('history', { title: 'History', list_of_page_attribute });
 });
 
 app.get('/setting', (req: Request, res: Response) => {
-    res.render('setting', { title: 'Setting' });
+    res.render('setting', { title: 'Setting', list_of_page_attribute });
 });
 
 app.listen(port, () => {console.log(`Listening in http://localhost:${port}`);});
