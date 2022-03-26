7fe567e259479d11992130bb1257e206cb90baec
NotHydra <rizky.irswanda115@gmail.com>
2022-03-26 14:29:54 +0800

    Change The Web Language To Indonesian, Some Adjustment To The Design & Begin Back End Development

--- dist/index.js
+++ dist/index.js
@@ -4,27 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
 app.set('view engine', 'ejs');
 app.use(express_1.default.static('public'));
 // app.use(express.urlencoded({ extended:false }));
 const port = process.env.PORT || 5000;
+const user_data = JSON.parse(fs_1.default.readFileSync('./json/users.json', 'utf-8'));
 app.get('/', (req, res) => {
     res.redirect('/home');
 });
 app.get('/home', (req, res) => {
-    res.render('home', { title: 'Home' });
+    res.render('home', { title: 'Utama', user_data });
 });
 app.get('/stats', (req, res) => {
-    res.render('stats', { title: 'Stats' });
+    res.render('stats', { title: 'Informasi', user_data });
 });
 app.get('/upgrade', (req, res) => {
-    res.render('upgrade', { title: 'Upgrade' });
+    res.render('upgrade', { title: 'Peningkatan', user_data });
 });
 app.get('/history', (req, res) => {
-    res.render('history', { title: 'History' });
+    res.render('history', { title: 'Berita', user_data });
 });
 app.get('/setting', (req, res) => {
-    res.render('setting', { title: 'Setting' });
+    res.render('setting', { title: 'Pengaturan', user_data });
 });
 app.listen(port, () => { console.log(`Listening in http://localhost:${port}`); });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+import fs from 'fs';
 
 const app: Application = express();
 app.set('view engine', 'ejs');
@@ -6,29 +7,30 @@ app.use(express.static('public'));
 // app.use(express.urlencoded({ extended:false }));
 
 const port: any = process.env.PORT || 5000;
+const user_data = JSON.parse(fs.readFileSync('./json/users.json', 'utf-8'));
 
 app.get('/', (req: Request, res: Response) => {
     res.redirect('/home');
 });
 
 app.get('/home', (req: Request, res: Response) => {
-    res.render('home', { title: 'Home' });
+    res.render('home', { title: 'Utama', user_data });
 });
 
 app.get('/stats', (req: Request, res: Response) => {
-    res.render('stats', { title: 'Stats' });
+    res.render('stats', { title: 'Informasi', user_data });
 });
 
 app.get('/upgrade', (req: Request, res: Response) => {
-    res.render('upgrade', { title: 'Upgrade' });
+    res.render('upgrade', { title: 'Peningkatan', user_data });
 });
 
 app.get('/history', (req: Request, res: Response) => {
-    res.render('history', { title: 'History' });
+    res.render('history', { title: 'Berita', user_data });
 });
 
 app.get('/setting', (req: Request, res: Response) => {
-    res.render('setting', { title: 'Setting' });
+    res.render('setting', { title: 'Pengaturan', user_data });
 });
 
 app.listen(port, () => {console.log(`Listening in http://localhost:${port}`);});
