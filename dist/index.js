a7c3c3c831bef574ea35ef3599604be972627901
NotHydra <rizky.irswanda115@gmail.com>
2022-03-27 21:57:39 +0800

    Favicon Added, New User Data System & Back-End Display For Home Page

--- dist/index.js
+++ dist/index.js
@@ -37,6 +37,7 @@ app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 app.use((0, method_override_1.default)('_method'));
 const port = process.env.PORT || 5000;
+const list_of_names = ["Dwi", "Nur", "Dewi", "Tri", "Dian", "Sri", "Putri", "Eka", "Sari", "Ayu", "Wahyu", "Indah", "Siti", "Ika", "Agus", "Fitri", "Ratna", "Andi", "Agung", "Ahmad", "Kurniawan", "Budi", "Adi", "Eko", "Nurul", "Putra", "Arif", "Puspita", "Ari", "Indra", "Dyah", "Rizki", "Maria", "Ratih", "Pratiwi", "Kartika", "Wulandari", "Fajar", "Bayu", "Lestari", "Anita", "Muhamad", "Kusuma", "Rahmawati", "Fitria", "Retno", "Kurnia", "Novita", "Aditya", "Ria", "Nugroho", "Putu", "Handayani", "Rahayu", "Yunita", "Rina", "Ade", "Widya", "Intan", "Diah", "Agustina", "Made", "Abdul", "Setiawan", "Rizky", "Rini", "Wahyuni", "Yulia", "Maya", "Puji", "Utami", "Amalia", "Dina", "Devi", "Citra", "Arief", "Bagus", "Hidayat", "Hendra", "Eva", "Endah", "Raden", "Novi", "Astuti", "Irma", "Achmad", "Aulia", "Surya", "Amelia", "Prima", "Angga", "Hadi", "Diana", "Anggraini", "Wulan", "Saputra", "Yuni", "Prasetyo", "Reza", "Suci"];
 const user_data = JSON.parse(fs_1.default.readFileSync('./json/users.json', 'utf-8'));
 const case_data = JSON.parse(fs_1.default.readFileSync('./json/cases.json', 'utf-8'));
 const initializePassport = require('./passport-config');
@@ -45,6 +46,7 @@ initializePassport(passport_1.default,
 username => user_data.find(user => user.username === username), 
 // @ts-ignore
 id => user_data.find(user => user.id === id));
+//#region Functions
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
@@ -57,6 +59,19 @@ function checkNotAuthenticated(req, res, next) {
     }
     next();
 }
+function getRandomNumber(min, max) {
+    max += 1;
+    let value = Math.floor(Math.random() * (max - min) + min);
+    if (value == max) {
+        value -= 1;
+    }
+    return value;
+}
+function getRandomCaseTheme(random_case_theme, case_rank) {
+    let random_case_number = getRandomNumber(0, case_data.length - 1);
+    return random_case_number;
+}
+//#endregion Functions
 //#region Main Request
 app.get('/', checkAuthenticated, (req, res) => {
     res.redirect('/home');
@@ -65,16 +80,16 @@ app.get('/home', checkAuthenticated, (req, res) => {
     res.render('home', { title: 'Utama', user_data: req.user });
 });
 app.get('/stats', checkAuthenticated, (req, res) => {
-    res.render('stats', { title: 'Informasi', user_data });
+    res.render('stats', { title: 'Informasi', user_data: req.user });
 });
 app.get('/upgrade', checkAuthenticated, (req, res) => {
-    res.render('upgrade', { title: 'Peningkatan', user_data });
+    res.render('upgrade', { title: 'Peningkatan', user_data: req.user });
 });
 app.get('/history', checkAuthenticated, (req, res) => {
-    res.render('history', { title: 'Berita', user_data });
+    res.render('history', { title: 'Berita', user_data: req.user });
 });
 app.get('/setting', checkAuthenticated, (req, res) => {
-    res.render('setting', { title: 'Pengaturan', user_data });
+    res.render('setting', { title: 'Pengaturan', user_data: req.user });
 });
 //#endregion Main Request
 //#region Authentication Request
@@ -90,16 +105,63 @@ app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register', { title: 'Register' });
 });
 app.post('/register', checkNotAuthenticated, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const hashedPassword = yield bcrypt_1.default.hash(req.body.password, 12);
-        let random_case_max = Math.ceil(Math.random() * 5);
-        if (random_case_max == 1 || random_case_max == 2) {
-            random_case_max = 3;
+    let username_is_valid = true;
+    for (let i = 0; i < user_data.length; i++) {
+        if (String(req.body.username) == user_data[i].username) {
+            username_is_valid = false;
+            break;
         }
-        ;
+    }
+    ;
+    if (username_is_valid) {
+        const temp_case_status = getRandomNumber(0, 3);
+        const hashedPassword = yield bcrypt_1.default.hash(req.body.password, 12);
+        const random_case_max = getRandomNumber(3, 5);
+        const random_case_name = list_of_names[getRandomNumber(0, 99)];
+        const random_case_age = getRandomNumber(19, 78);
+        let case_is_guilty;
+        if (temp_case_status == 0) {
+            case_is_guilty = false;
+        }
+        else if (temp_case_status == 1 || temp_case_status == 2 || temp_case_status == 3) {
+            case_is_guilty = true;
+        }
+        let random_case_theme = [];
+        let case_rank = [];
+        if (!case_is_guilty) {
+            let random_case_number = getRandomCaseTheme(random_case_theme, case_rank);
+            random_case_theme.push(case_data[random_case_number]);
+            case_rank.push(case_data[random_case_number].rank);
+        }
+        else if (case_is_guilty) {
+            let random_case_number = [];
+            for (let i = 0; i < 4; i++) {
+                let temp_random_case_number = getRandomCaseTheme(random_case_theme, case_rank);
+                let random_case_number_is_valid = true;
+                for (let j = 0; j < random_case_number.length; j++) {
+                    if (temp_random_case_number == random_case_number[j]) {
+                        random_case_number_is_valid = false;
+                    }
+                }
+                if (random_case_number_is_valid) {
+                    random_case_number.push(temp_random_case_number);
+                }
+                else if (!random_case_number_is_valid) {
+                    i -= 1;
+                }
+            }
+            let random_case_theme_value_range = getRandomNumber(1, 100);
+            let case_theme_range = [0, 25, 50, 75];
+            for (let i = 0; i < random_case_number.length; i++) {
+                if (random_case_theme_value_range > case_theme_range[i]) {
+                    random_case_theme.push(case_data[random_case_number[i]]);
+                    case_rank.push(case_data[random_case_number[i]].rank);
+                }
+            }
+        }
         user_data.push({
-            "id": 2,
-            "username": req.body.username,
+            "id": user_data.length + 1,
+            "username": String(req.body.username),
             "password": hashedPassword,
             "people_current": 50,
             "people_max": 100,
@@ -109,8 +171,6 @@ app.post('/register', checkNotAuthenticated, (req, res) => __awaiter(void 0, voi
             "crime_rate_max": 100,
             "money_current": 50,
             "money_max": 100,
-            "current_day": 1,
-            "total_crime_case": 1,
             "crime_case": [
                 {
                     "day": 1,
@@ -119,21 +179,24 @@ app.post('/register', checkNotAuthenticated, (req, res) => __awaiter(void 0, voi
                     "case": [
                         {
                             "case_id": 1,
-                            "case_theme": [1, 2, 3],
-                            "case_name": "name1",
-                            "case_age": "age1",
+                            "case_theme": random_case_theme,
+                            "case_name": random_case_name,
+                            "case_age": random_case_age,
+                            "case_is_guilty": case_is_guilty,
+                            "crime_rank": Math.max.apply(null, case_rank),
                             "case_done": false
                         }
                     ]
                 }
             ]
         });
+        fs_1.default.writeFileSync('./json/users.json', JSON.stringify(user_data, null, 4));
         res.redirect('/login');
     }
-    catch (_a) {
+    else if (!username_is_valid) {
+        req.flash('info', 'Username already taken');
         res.redirect('/register');
     }
-    console.log(user_data);
 }));
 app.get('/logout', checkAuthenticated, (req, res) => {
     res.render('logout', { title: 'Logout' });
--- src/index.ts
+++ src/index.ts
@@ -26,8 +26,9 @@ app.use(passport.session())
 app.use(methodOverride('_method'))
 
 const port: any = process.env.PORT || 5000;
-const user_data = JSON.parse(fs.readFileSync('./json/users.json', 'utf-8'));
-const case_data = JSON.parse(fs.readFileSync('./json/cases.json', 'utf-8'));
+const list_of_names: Array<string> = ["Dwi", "Nur", "Dewi", "Tri", "Dian", "Sri", "Putri", "Eka", "Sari", "Ayu", "Wahyu", "Indah", "Siti", "Ika", "Agus", "Fitri", "Ratna", "Andi", "Agung", "Ahmad", "Kurniawan", "Budi", "Adi", "Eko", "Nurul", "Putra", "Arif", "Puspita", "Ari", "Indra", "Dyah", "Rizki", "Maria", "Ratih", "Pratiwi", "Kartika", "Wulandari", "Fajar", "Bayu", "Lestari", "Anita", "Muhamad", "Kusuma", "Rahmawati", "Fitria", "Retno", "Kurnia", "Novita", "Aditya", "Ria", "Nugroho", "Putu", "Handayani", "Rahayu", "Yunita", "Rina", "Ade", "Widya", "Intan", "Diah", "Agustina", "Made", "Abdul", "Setiawan", "Rizky", "Rini", "Wahyuni", "Yulia", "Maya", "Puji", "Utami", "Amalia", "Dina", "Devi", "Citra", "Arief", "Bagus", "Hidayat", "Hendra", "Eva", "Endah", "Raden", "Novi", "Astuti", "Irma", "Achmad", "Aulia", "Surya", "Amelia", "Prima", "Angga", "Hadi", "Diana", "Anggraini", "Wulan", "Saputra", "Yuni", "Prasetyo", "Reza", "Suci"]
+const user_data: any = JSON.parse(fs.readFileSync('./json/users.json', 'utf-8'));
+const case_data: any = JSON.parse(fs.readFileSync('./json/cases.json', 'utf-8'));
 
 const initializePassport = require('./passport-config')
 initializePassport(
@@ -38,6 +39,7 @@ initializePassport(
     id => user_data.find(user => user.id === id)
 )
 
+//#region Functions
 function checkAuthenticated(req: Request, res: Response, next: NextFunction){
     if(req.isAuthenticated()){
         return next()
@@ -54,6 +56,26 @@ function checkNotAuthenticated(req: Request, res: Response, next: NextFunction){
     next()
 }
 
+function getRandomNumber(min: number, max: number){
+    max += 1
+
+    let value = Math.floor(Math.random() * (max - min) + min);
+
+    if(value == max){
+        value -= 1
+    }
+
+    return value
+}
+
+function getRandomCaseTheme(random_case_theme: any, case_rank: any){
+    let random_case_number = getRandomNumber(0, case_data.length - 1)
+
+    return random_case_number;
+}
+
+//#endregion Functions
+
 //#region Main Request
 app.get('/', checkAuthenticated, (req: Request, res: Response) => {
     res.redirect('/home');
@@ -64,19 +86,19 @@ app.get('/home', checkAuthenticated, (req: Request, res: Response) => {
 });
 
 app.get('/stats', checkAuthenticated, (req: Request, res: Response) => {
-    res.render('stats', { title: 'Informasi', user_data });
+    res.render('stats', { title: 'Informasi', user_data: req.user });
 });
 
 app.get('/upgrade', checkAuthenticated, (req: Request, res: Response) => {
-    res.render('upgrade', { title: 'Peningkatan', user_data });
+    res.render('upgrade', { title: 'Peningkatan', user_data: req.user });
 });
 
 app.get('/history', checkAuthenticated, (req: Request, res: Response) => {
-    res.render('history', { title: 'Berita', user_data });
+    res.render('history', { title: 'Berita', user_data: req.user });
 });
 
 app.get('/setting', checkAuthenticated, (req: Request, res: Response) => {
-    res.render('setting', { title: 'Pengaturan', user_data });
+    res.render('setting', { title: 'Pengaturan', user_data: req.user });
 });
 
 //#endregion Main Request
@@ -97,17 +119,76 @@ app.get('/register', checkNotAuthenticated, (req: Request, res: Response) => {
 });
 
 app.post('/register', checkNotAuthenticated, async (req: Request, res: Response) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    let username_is_valid = true
+    for(let i = 0; i < user_data.length; i++){
+        if(String(req.body.username) == user_data[i].username){
+            username_is_valid = false;
+            break;
+        }
+    };
 
-        let random_case_max =  Math.ceil(Math.random() * 5);
-        if(random_case_max == 1 || random_case_max == 2){
-            random_case_max = 3;
-        };
+    if(username_is_valid){
+        const temp_case_status: number = getRandomNumber(0, 3);
+
+        const hashedPassword: any = await bcrypt.hash(req.body.password, 12);
+        const random_case_max: number = getRandomNumber(3, 5);
+        const random_case_name: string = list_of_names[getRandomNumber(0, 99)];
+        const random_case_age: number = getRandomNumber(19, 78);
+
+        let case_is_guilty;
+        if(temp_case_status == 0){
+            case_is_guilty = false;
+        }
+        
+        else if(temp_case_status == 1 || temp_case_status == 2 || temp_case_status == 3){
+            case_is_guilty = true;
+        }
+
+        let random_case_theme: any = [];
+        let case_rank: any = [];
+        if(!case_is_guilty){
+            let random_case_number = getRandomCaseTheme(random_case_theme, case_rank)
+
+            random_case_theme.push(case_data[random_case_number])
+            case_rank.push(case_data[random_case_number].rank)
+        }
+
+        else if(case_is_guilty){
+            let random_case_number = [];
+
+            for(let i = 0; i < 4; i++){
+                let temp_random_case_number = getRandomCaseTheme(random_case_theme, case_rank)
+
+                let random_case_number_is_valid = true
+                for(let j = 0; j < random_case_number.length; j++){
+                    if(temp_random_case_number == random_case_number[j]){
+                        random_case_number_is_valid = false
+                    }
+                }
+
+                if(random_case_number_is_valid){
+                    random_case_number.push(temp_random_case_number)
+                }
+
+                else if (!random_case_number_is_valid){
+                    i -= 1;
+                }
+            }
+
+            let random_case_theme_value_range = getRandomNumber(1, 100)
+            let case_theme_range = [0, 25, 50, 75]
+
+            for(let i = 0; i < random_case_number.length; i++){
+                if(random_case_theme_value_range > case_theme_range[i]){
+                    random_case_theme.push(case_data[random_case_number[i]])
+                    case_rank.push(case_data[random_case_number[i]].rank)
+                }
+            }
+        }
 
         user_data.push({
-            "id": 2,
-            "username": req.body.username,
+            "id": user_data.length + 1,
+            "username": String(req.body.username),
             "password": hashedPassword,
             "people_current": 50,
             "people_max": 100,
@@ -117,8 +198,6 @@ app.post('/register', checkNotAuthenticated, async (req: Request, res: Response)
             "crime_rate_max": 100,
             "money_current": 50,
             "money_max": 100,
-            "current_day": 1,
-            "total_crime_case" : 1,
             "crime_case": [
                 {
                     "day": 1,
@@ -127,9 +206,11 @@ app.post('/register', checkNotAuthenticated, async (req: Request, res: Response)
                     "case": [
                         {
                             "case_id": 1,
-                            "case_theme": [1, 2, 3],
-                            "case_name": "name1",
-                            "case_age": "age1",
+                            "case_theme": random_case_theme,
+                            "case_name": random_case_name,
+                            "case_age": random_case_age,
+                            "case_is_guilty": case_is_guilty,
+                            "crime_rank": Math.max.apply(null, case_rank),
                             "case_done": false
                         }
                     ]
@@ -137,14 +218,15 @@ app.post('/register', checkNotAuthenticated, async (req: Request, res: Response)
             ]
         });
 
+        fs.writeFileSync('./json/users.json', JSON.stringify(user_data, null, 4))
+
         res.redirect('/login');
     }
 
-    catch {
-        res.redirect('/register');
+    else if(!username_is_valid){
+        req.flash('info', 'Username already taken')
+        res.redirect('/register')
     }
-
-    console.log(user_data)
 });
 
 app.get('/logout', checkAuthenticated, (req: Request, res: Response) => {
