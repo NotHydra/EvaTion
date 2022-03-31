91c416d0e775765b3339c05bc1ae71d366d53b37
NotHydra <rizky.irswanda115@gmail.com>
2022-03-31 12:52:31 +0800

    Python & Excel Utility

---
+++ python/main.py
@@ -0,0 +1,29 @@
+from excel import Excel
+import json
+
+class Main():
+    def main():
+        wb_cases = Excel("excel/cases.xlsx")
+
+        value = wb_cases.get_value_multiple_2d("A2", "F61")
+        json_array = []
+
+        for i in value:
+            temp_json_dictionary = {
+                "id": i[0],
+                "crime": i[1],
+                "conclusion": i[2],
+                "identity" : eval(i[3]),
+                "case_is_guilty" : i[4],
+                "rank": i[5]
+            }
+
+            json_array.append(temp_json_dictionary)
+
+        json_object = json.dumps(json_array, indent = 4)
+
+        with open("json/cases.json", "w") as outfile:
+            outfile.write(json_object)
+
+if(__name__ == "__main__"):
+    Main.main()
