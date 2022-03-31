82bb5139c9892de84fee4c536454246c79e1272f
NotHydra <rizky.irswanda115@gmail.com>
2022-03-31 20:35:45 +0800

    Fixed Indetity Type Error For Cases & Adjust Text For Cases

--- python/main.py
+++ python/main.py
@@ -5,15 +5,18 @@ class Main():
     def main():
         wb_cases = Excel("excel/cases.xlsx")
 
-        value = wb_cases.get_value_multiple_2d("A2", "F61")
+        value = wb_cases.get_value_multiple_2d("A2", "F31")
         json_array = []
 
         for i in value:
+            if(type(i[3]) == str):
+                i[3] = eval(i[3])
+
             temp_json_dictionary = {
                 "id": i[0],
                 "crime": i[1],
                 "conclusion": i[2],
-                "identity" : eval(i[3]),
+                "identity" : i[3],
                 "case_is_guilty" : i[4],
                 "rank": i[5]
             }
