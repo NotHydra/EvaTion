889a4b70963d9c7c4d598e74085e769a33a6af5d
NotHydra <rizky.irswanda115@gmail.com>
2022-04-06 16:11:03 +0800

    Added More Randomness To People, Prosperity and Crime Rate Stats & TS Code Adjustment

--- dist/index.js
+++ dist/index.js
@@ -37,18 +37,6 @@ app.use((0, express_session_1.default)({ secret: process.env.SESSION_SECRET, res
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 app.use((0, method_override_1.default)('_method'));
-const port = process.env.PORT || 5000;
-const special_char = /[!-#%-*,-\/:;?@[-\]_{}\u00A1\u00A7\u00AB\u00B6\u00B7\u00BB\u00BF\u037E\u0387\u055A-\u055F\u0589\u058A\u05BE\u05C0\u05C3\u05C6\u05F3\u05F4\u0609\u060A\u060C\u060D\u061B\u061E\u061F\u066A-\u066D\u06D4\u0700-\u070D\u07F7-\u07F9\u0830-\u083E\u085E\u0964\u0965\u0970\u09FD\u0A76\u0AF0\u0C84\u0DF4\u0E4F\u0E5A\u0E5B\u0F04-\u0F12\u0F14\u0F3A-\u0F3D\u0F85\u0FD0-\u0FD4\u0FD9\u0FDA\u104A-\u104F\u10FB\u1360-\u1368\u1400\u166D\u166E\u169B\u169C\u16EB-\u16ED\u1735\u1736\u17D4-\u17D6\u17D8-\u17DA\u1800-\u180A\u1944\u1945\u1A1E\u1A1F\u1AA0-\u1AA6\u1AA8-\u1AAD\u1B5A-\u1B60\u1BFC-\u1BFF\u1C3B-\u1C3F\u1C7E\u1C7F\u1CC0-\u1CC7\u1CD3\u2010-\u2027\u2030-\u2043\u2045-\u2051\u2053-\u205E\u207D\u207E\u208D\u208E\u2308-\u230B\u2329\u232A\u2768-\u2775\u27C5\u27C6\u27E6-\u27EF\u2983-\u2998\u29D8-\u29DB\u29FC\u29FD\u2CF9-\u2CFC\u2CFE\u2CFF\u2D70\u2E00-\u2E2E\u2E30-\u2E4E\u3001-\u3003\u3008-\u3011\u3014-\u301F\u3030\u303D\u30A0\u30FB\uA4FE\uA4FF\uA60D-\uA60F\uA673\uA67E\uA6F2-\uA6F7\uA874-\uA877\uA8CE\uA8CF\uA8F8-\uA8FA\uA8FC\uA92E\uA92F\uA95F\uA9C1-\uA9CD\uA9DE\uA9DF\uAA5C-\uAA5F\uAADE\uAADF\uAAF0\uAAF1\uABEB\uFD3E\uFD3F\uFE10-\uFE19\uFE30-\uFE52\uFE54-\uFE61\uFE63\uFE68\uFE6A\uFE6B\uFF01-\uFF03\uFF05-\uFF0A\uFF0C-\uFF0F\uFF1A\uFF1B\uFF1F\uFF20\uFF3B-\uFF3D\uFF3F\uFF5B\uFF5D\uFF5F-\uFF65\u{10100}-\u{10102}\u{1039F}\u{103D0}\u{1056F}\u{10857}\u{1091F}\u{1093F}\u{10A50}-\u{10A58}\u{10A7F}\u{10AF0}-\u{10AF6}\u{10B39}-\u{10B3F}\u{10B99}-\u{10B9C}\u{10F55}-\u{10F59}\u{11047}-\u{1104D}\u{110BB}\u{110BC}\u{110BE}-\u{110C1}\u{11140}-\u{11143}\u{11174}\u{11175}\u{111C5}-\u{111C8}\u{111CD}\u{111DB}\u{111DD}-\u{111DF}\u{11238}-\u{1123D}\u{112A9}\u{1144B}-\u{1144F}\u{1145B}\u{1145D}\u{114C6}\u{115C1}-\u{115D7}\u{11641}-\u{11643}\u{11660}-\u{1166C}\u{1173C}-\u{1173E}\u{1183B}\u{11A3F}-\u{11A46}\u{11A9A}-\u{11A9C}\u{11A9E}-\u{11AA2}\u{11C41}-\u{11C45}\u{11C70}\u{11C71}\u{11EF7}\u{11EF8}\u{12470}-\u{12474}\u{16A6E}\u{16A6F}\u{16AF5}\u{16B37}-\u{16B3B}\u{16B44}\u{16E97}-\u{16E9A}\u{1BC9F}\u{1DA87}-\u{1DA8B}\u{1E95E}\u{1E95F}]/u;
-const list_of_name = {
-    "male": ["Putra", "Wahyu", "Agus", "Agung", "Ahmad", "Kurniawan", "Budi", "Adi", "Eko", "Arief", "Ari", "Indra", "Rizki", "Yusuf", "Fajar", "Bayu", "Aditya", "Nugroho", "Abdul", "Setiawan", "Riski", "Bagus", "Hidayat", "Rian", "Hendra", "Raden", "Surya", "Angga", "Hadi", "Adam", "Rudi", "Andri", "Taufik", "Hanif"],
-    "female": ["Nur", "Dewi", "Dian", "Sri", "Putri", "Sari", "Ayu", "Indah", "Siti", "Fitri", "Ratna", "Puspita", "Ratih", "Pratiwi", "Tika", "Wulandari", "Lestari", "Febri", "Anita", "Rahma", "Fitria", "Novi", "Ria", "Rahayu", "Yunita", "Rina", "Widya", "Intan", "Agustina", "Rini", "Yulia", "Maya", "Utami", "Amel", "Devi", "Citra", "Diana", "Wulan", "Yuni", "Sinta", "Cantika"]
-};
-const dbURI = process.env.dbURI;
-mongoose_1.default.connect(process.env.MONGODB_URI || dbURI)
-    .then(() => {
-    app.listen(port, () => { console.log(`Listening in http://localhost:${port}`); });
-})
-    .catch((err) => console.log(err));
 function find_data() {
     case_model_1.default.find().sort({ id: 'asc' })
         .then((result) => {
@@ -154,9 +142,10 @@ function main(case_datas, user_datas) {
                     crime_rate_increase += current_case_rank * 5;
                 }
                 else if (current_case_rank == 0) {
-                    crime_rate_increase += get_random_number(5, 15);
+                    crime_rate_increase += get_random_number(10, 20);
                 }
                 ;
+                crime_rate_increase += get_random_number(-5, 5);
             }
             ;
             crime_case_data[current_day_length].case_current += 1;
@@ -210,10 +199,10 @@ function main(case_datas, user_datas) {
                     ]
                 });
                 crime_case_taken.push(random_case_theme);
+                console.log(`Crime Rate Increase    : ${crime_rate_increase}`);
                 let random_crime_rate_decrease = get_random_number(5, 10);
                 user_request.crime_rate_current += crime_rate_increase;
                 user_request.crime_rate_current -= random_crime_rate_decrease;
-                console.log(crime_rate_increase, random_crime_rate_decrease);
                 crime_rate_increase = 0;
                 if (user_request.crime_rate_current < 0) {
                     user_request.crime_rate_current = 0;
@@ -223,8 +212,13 @@ function main(case_datas, user_datas) {
                     user_request.crime_rate_current = user_request.crime_rate_max;
                 }
                 ;
-                user_request.people_current += user_request.people_max * (10 / 100);
-                user_request.people_current -= (user_request.people_current * (user_request.crime_rate_current / 100)) / 2;
+                let user_people_current_increase = user_request.people_max * (10 / 100);
+                let user_people_current_decrease = (user_request.people_current * (user_request.crime_rate_current / 100)) / 2;
+                let user_people_current_base_random_value = Math.trunc(user_people_current_increase / 4);
+                let user_people_current_random = get_random_number(-1 * user_people_current_base_random_value, user_people_current_base_random_value);
+                user_request.people_current += user_people_current_increase;
+                user_request.people_current += user_people_current_random;
+                user_request.people_current -= user_people_current_decrease;
                 if (user_request.people_current < 0) {
                     user_request.people_current = 0;
                 }
@@ -233,8 +227,13 @@ function main(case_datas, user_datas) {
                     user_request.people_current = user_request.people_max;
                 }
                 ;
-                user_request.prosperity_current += user_request.prosperity_max * (((100 - user_request.crime_rate_current) / 100) / 4);
-                user_request.prosperity_current -= user_request.prosperity_current * ((user_request.crime_rate_current / 100) / 2);
+                let user_prosperity_current_increase = user_request.prosperity_max * (((100 - user_request.crime_rate_current) / 100) / 4);
+                let user_prosperity_current_decrease = user_request.prosperity_current * ((user_request.crime_rate_current / 100) / 2);
+                let user_prosperity_current_base_random_value = Math.trunc(user_prosperity_current_increase / 4);
+                let user_prosperity_current_random = get_random_number(-1 * user_prosperity_current_base_random_value, user_prosperity_current_base_random_value);
+                user_request.prosperity_current += user_prosperity_current_increase;
+                user_request.prosperity_current += user_prosperity_current_random;
+                user_request.prosperity_current -= user_prosperity_current_decrease;
                 if (user_request.prosperity_current < 0) {
                     user_request.prosperity_current = 0;
                 }
@@ -243,6 +242,17 @@ function main(case_datas, user_datas) {
                     user_request.prosperity_current = user_request.prosperity_max;
                 }
                 ;
+                // console.log(`Crime Rate Decrease    : ${random_crime_rate_decrease}`);
+                // console.log()
+                // console.log(`People Increase        : ${user_people_current_increase}`);
+                // console.log(`People Decrease        : ${user_people_current_decrease}`);
+                // console.log(`People Base Random     : ${user_people_current_base_random_value}`);
+                // console.log(`People Random          : ${user_people_current_random}`);
+                // console.log()
+                // console.log(`Prosperity Increase    : ${user_prosperity_current_increase}`);
+                // console.log(`Prosperity Decrease    : ${user_prosperity_current_decrease}`);
+                // console.log(`Prosperity Base Random : ${user_prosperity_current_base_random_value}`);
+                // console.log(`Prosperity Random      : ${user_prosperity_current_random}`);
             }
             res.redirect('/home');
         }
@@ -460,4 +470,16 @@ function main(case_datas, user_datas) {
     });
     //#endregion Authentication Request
 }
-find_data();
+const port = process.env.PORT || 5000;
+const special_char = /[!-#%-*,-\/:;?@[-\]_{}\u00A1\u00A7\u00AB\u00B6\u00B7\u00BB\u00BF\u037E\u0387\u055A-\u055F\u0589\u058A\u05BE\u05C0\u05C3\u05C6\u05F3\u05F4\u0609\u060A\u060C\u060D\u061B\u061E\u061F\u066A-\u066D\u06D4\u0700-\u070D\u07F7-\u07F9\u0830-\u083E\u085E\u0964\u0965\u0970\u09FD\u0A76\u0AF0\u0C84\u0DF4\u0E4F\u0E5A\u0E5B\u0F04-\u0F12\u0F14\u0F3A-\u0F3D\u0F85\u0FD0-\u0FD4\u0FD9\u0FDA\u104A-\u104F\u10FB\u1360-\u1368\u1400\u166D\u166E\u169B\u169C\u16EB-\u16ED\u1735\u1736\u17D4-\u17D6\u17D8-\u17DA\u1800-\u180A\u1944\u1945\u1A1E\u1A1F\u1AA0-\u1AA6\u1AA8-\u1AAD\u1B5A-\u1B60\u1BFC-\u1BFF\u1C3B-\u1C3F\u1C7E\u1C7F\u1CC0-\u1CC7\u1CD3\u2010-\u2027\u2030-\u2043\u2045-\u2051\u2053-\u205E\u207D\u207E\u208D\u208E\u2308-\u230B\u2329\u232A\u2768-\u2775\u27C5\u27C6\u27E6-\u27EF\u2983-\u2998\u29D8-\u29DB\u29FC\u29FD\u2CF9-\u2CFC\u2CFE\u2CFF\u2D70\u2E00-\u2E2E\u2E30-\u2E4E\u3001-\u3003\u3008-\u3011\u3014-\u301F\u3030\u303D\u30A0\u30FB\uA4FE\uA4FF\uA60D-\uA60F\uA673\uA67E\uA6F2-\uA6F7\uA874-\uA877\uA8CE\uA8CF\uA8F8-\uA8FA\uA8FC\uA92E\uA92F\uA95F\uA9C1-\uA9CD\uA9DE\uA9DF\uAA5C-\uAA5F\uAADE\uAADF\uAAF0\uAAF1\uABEB\uFD3E\uFD3F\uFE10-\uFE19\uFE30-\uFE52\uFE54-\uFE61\uFE63\uFE68\uFE6A\uFE6B\uFF01-\uFF03\uFF05-\uFF0A\uFF0C-\uFF0F\uFF1A\uFF1B\uFF1F\uFF20\uFF3B-\uFF3D\uFF3F\uFF5B\uFF5D\uFF5F-\uFF65\u{10100}-\u{10102}\u{1039F}\u{103D0}\u{1056F}\u{10857}\u{1091F}\u{1093F}\u{10A50}-\u{10A58}\u{10A7F}\u{10AF0}-\u{10AF6}\u{10B39}-\u{10B3F}\u{10B99}-\u{10B9C}\u{10F55}-\u{10F59}\u{11047}-\u{1104D}\u{110BB}\u{110BC}\u{110BE}-\u{110C1}\u{11140}-\u{11143}\u{11174}\u{11175}\u{111C5}-\u{111C8}\u{111CD}\u{111DB}\u{111DD}-\u{111DF}\u{11238}-\u{1123D}\u{112A9}\u{1144B}-\u{1144F}\u{1145B}\u{1145D}\u{114C6}\u{115C1}-\u{115D7}\u{11641}-\u{11643}\u{11660}-\u{1166C}\u{1173C}-\u{1173E}\u{1183B}\u{11A3F}-\u{11A46}\u{11A9A}-\u{11A9C}\u{11A9E}-\u{11AA2}\u{11C41}-\u{11C45}\u{11C70}\u{11C71}\u{11EF7}\u{11EF8}\u{12470}-\u{12474}\u{16A6E}\u{16A6F}\u{16AF5}\u{16B37}-\u{16B3B}\u{16B44}\u{16E97}-\u{16E9A}\u{1BC9F}\u{1DA87}-\u{1DA8B}\u{1E95E}\u{1E95F}]/u;
+const list_of_name = {
+    "male": ["Putra", "Wahyu", "Agus", "Agung", "Ahmad", "Kurniawan", "Budi", "Adi", "Eko", "Arief", "Ari", "Indra", "Rizki", "Yusuf", "Fajar", "Bayu", "Aditya", "Nugroho", "Abdul", "Setiawan", "Riski", "Bagus", "Hidayat", "Rian", "Hendra", "Raden", "Surya", "Angga", "Hadi", "Adam", "Rudi", "Andri", "Taufik", "Hanif"],
+    "female": ["Nur", "Dewi", "Dian", "Sri", "Putri", "Sari", "Ayu", "Indah", "Siti", "Fitri", "Ratna", "Puspita", "Ratih", "Pratiwi", "Tika", "Wulandari", "Lestari", "Febri", "Anita", "Rahma", "Fitria", "Novi", "Ria", "Rahayu", "Yunita", "Rina", "Widya", "Intan", "Agustina", "Rini", "Yulia", "Maya", "Utami", "Amel", "Devi", "Citra", "Diana", "Wulan", "Yuni", "Sinta", "Cantika"]
+};
+const dbURI = process.env.dbURI;
+mongoose_1.default.connect(process.env.MONGODB_URI || dbURI)
+    .then(() => {
+    app.listen(port, () => { console.log(`Listening in http://localhost:${port}`); });
+    find_data();
+})
+    .catch((err) => console.log(err));
